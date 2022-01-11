46c8699d394316f3706609126253157d8e3faf34
june <ryu9663@naver.com>
2022-01-11 16:48:50 +0900

    feat 체크박스 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoListItem from "./TodoListItem";
 
-function App() {
+const initialTodos: Array<Todo> = [
+  { id: 0, text: "Walk the dog", complete: true },
+  { id: 1, text: "Write app", complete: false },
+  { id: 2, text: "Hangout with friend", complete: false },
+];
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
+  const toggleTodo: ToggleTodo = (selectedTodo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      } else return todo;
+    });
+    setTodos(newTodos);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <TodoListItem todo={todos[0]} /> */}
+      {todos.map((todo) => {
+        return (
+          <React.Fragment key={todo.id}>
+            <TodoListItem todo={todo} toggleTodo={toggleTodo} />
+          </React.Fragment>
+        );
+      })}
+    </>
   );
-}
-
+};
 export default App;
